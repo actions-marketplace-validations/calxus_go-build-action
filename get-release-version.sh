17d573bccaaf26c99a6ef3d816d251530f03fb88
Gordon Adam-Santiago <gtadam@protonmail.ch>
2020-12-17 11:05:11 +0000

    Debugging fetching of version number

--- get-release-version.sh
+++ get-release-version.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+curl -s https://api.github.com/repos/gtadam/$2/releases/latest
+curl -s https://api.github.com/repos/gtadam/$2/releases/latest | jq -r '.tag_name'
 export CURRENT_VERSION=$(curl -s https://api.github.com/repos/gtadam/$2/releases/latest | jq -r '.tag_name')
 export WORKDIR=$(dirname "$0")
 echo $($WORKDIR/increment_version.sh $1 $CURRENT_VERSION) > release_version.txt
