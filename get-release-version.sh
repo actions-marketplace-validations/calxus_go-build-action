2fd17b123177b4efcc09122dd5a4dbbb78a28022
Gordon Adam-Santiago <gtadam@protonmail.ch>
2020-12-17 10:51:33 +0000

    Debugging fetching of version number

--- get-release-version.sh
+++ get-release-version.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+curl -s https://api.github.com/repos/gtadam/kubernetes-deploy-action/releases/latest
+curl -s https://api.github.com/repos/gtadam/kubernetes-deploy-action/releases/latest | jq -r '.tag_name'
 export CURRENT_VERSION=$(curl -s https://api.github.com/repos/gtadam/kubernetes-deploy-action/releases/latest | jq -r '.tag_name')
 export WORKDIR=$(dirname "$0")
 echo $($WORKDIR/increment_version.sh $1 $CURRENT_VERSION) > release_version.txt
